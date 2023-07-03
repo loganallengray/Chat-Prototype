30868ccb936df867f91ee30c74e424860a5e1827
Logan Gray <loganallengray@gmail.com>
2023-07-03 17:53:52 -0500

    add chat button working

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import AppView from "./components/AppView";
 import Header from "./components/header/Header";
 import ChatContext from "./context/chat-context"
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [chats, setChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState(0);
+
   return (
     <div id="app">
       <Header />
-      <ChatContext.Provider value={[{ id: 1, text: "test" }]}>
+      <ChatContext.Provider value={
+        {
+          currentChat: currentChat,
+          setCurrentChat: setCurrentChat,
+          chats: chats,
+          setChats: setChats
+        }
+      }>
         <AppView />
       </ChatContext.Provider>
     </div>
--- src/components/chat/chatSidebar/ChatSidebar.js
+++ src/components/chat/chatSidebar/ChatSidebar.js
@@ -1,10 +1,26 @@
 import styles from './ChatSidebar.module.css';
+import React, { useContext } from 'react';
+import ChatContext from '../../../context/chat-context';
 
 const ChatSidebar = () => {
+    const cxt = useContext(ChatContext);
+
+    const newChatHandler = (event) => {
+        const newChats = [...cxt.chats];
+
+        newChats.push({
+            id: 1,
+            name: "test",
+            messages: []
+        })
+
+        cxt.setChats(newChats);
+    }
+
     return (
         <div id={styles.sidebar}>
             <div id={styles.sidebarHead}>
-                <div id={styles.sidebarHeadNew}>New Chat</div>
+                <div id={styles.sidebarHeadNew} onClick={newChatHandler}>New Chat</div>
                 <div id={styles.sidebarHeadClose}>Close</div>
             </div>
         </div>
