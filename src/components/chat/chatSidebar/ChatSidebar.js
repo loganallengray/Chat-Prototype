34320d52da69662196e8d9735101260a24f35f5a
Logan Gray <loganallengray@gmail.com>
2023-07-12 17:21:58 -0500

    border button created

--- src/components/chat/chatSidebar/ChatSidebar.js
+++ src/components/chat/chatSidebar/ChatSidebar.js
@@ -2,6 +2,7 @@ import styles from './ChatSidebar.module.css';
 import React, { useContext, useState } from 'react';
 import ChatContext from '../../../context/chat-context';
 import ChatList from './chatList/ChatList';
+import BorderButton from '../../../UI/buttons/BorderButton';
 
 const ChatSidebar = () => {
     const cxt = useContext(ChatContext);
@@ -28,15 +29,15 @@ const ChatSidebar = () => {
         return (
             <div id={styles.sidebar} className="mobile-sidebar">
                 <div id={styles.sidebarHead}>
-                    <div id={styles.newChat} className={styles.headButton} onClick={newChatHandler}>New Chat</div>
-                    <div id={styles.closeSidebar} className={`${styles.headButton} mobile-hideSidebarButton`} onClick={cxt.toggleShowSidebar}>Close</div>
+                    <BorderButton id={styles.newChat} onClick={newChatHandler}>New Chat</BorderButton>
+                    <BorderButton id={styles.closeSidebar} className="mobile-hideSidebarButton" onClick={cxt.toggleShowSidebar}>Close</BorderButton>
                 </div>
                 <ChatList />
             </div>
         )
     } else {
         return (
-            <div id={styles.openSidebar} className={`${styles.headButton} mobile-hideSidebarButton`} onClick={cxt.toggleShowSidebar}>Open</div>
+            <BorderButton id={styles.openSidebar} className={`${styles.headButton} mobile-hideSidebarButton`} onClick={cxt.toggleShowSidebar}>Open</BorderButton>
         )
     }
 }
